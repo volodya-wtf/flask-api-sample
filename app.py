5397a67c0f2d6aaed78882f3c5974b077c020cb7
Vladimir Petrov <volodya.workflow@gmail.com>
2021-12-09 16:24:00 +0700

    Complete

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
+
 import sqlite3
+
 
 app = Flask(__name__)
 
@@ -10,7 +14,7 @@ def db_connection():
         conn = sqlite3.connect("students.sqlite")
     except sqlite3.error as e:
         print(e)
-    return e
+    return conn
 
 
 @app.route("/students", methods=["POST", "GET"])
@@ -18,12 +22,67 @@ def students():
     conn = db_connection()
     cursor = conn.cursor()
 
+    if request.method == "GET":
+        cursor = conn.execute("SELECT * FROM students")
+        students = [
+            dict(id=row[0], firstname=row[1], lastname=row[2], telegram=row[4])
+            for row in cursor.fetchall()
+        ]
+        if students is not None:
+            return jsonify(students)
 
-@app.route('/student/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    if request.method == "POST":
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+        telegram = request.form["telegram"]
+
+        sql = """INSERT INTO students (firstname, lastname, telegram)
+        VALUES (?, ?, ?)"""
+        cursor = cursor.execute(sql, (firstname, lastname, telegram))
+        conn.commit()
+
+        return f"Student with id: {cursor.lastrowid} created successfully"
+
+
+@app.route("/student/<int:id>", methods=["GET", "PUT", "DELETE"])
 def student(id):
     conn = db_connection()
     cursor = conn.cursor()
     student = None
+
+    if request.method == "GET":
+        cursor.execute("SELECT * FROM students WHERE id=?", (id,))
+        rows = cursor.fetchall()
+        for row in rows:
+            student = row
+        if student is not None:
+            return jsonify(student), 200
+        else:
+            return "Something went wrong", 404
+
+    if request.method == "PUT":
+        sql = """UPDATE students SET firstname = ?, lastname = ?, telegram = ? WHERE id = ?"""
+
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+        telegram = request.form["telegram"]
+        updated_student = {
+            "id": id,
+            "firstname": firstname,
+            "lastname": lastname,
+        }
+
+        conn.execute(sql, (firstname, lastname, telegram, id))
+        conn.commit()
+
+        return jsonify(updated_student)
+
+    if request.method == "DELETE":
+        sql = """ DELETE FROM students WHERE id=? """
+        conn.execute(sql, (id,))
+        conn.commit()
+
+        return f"Student with id: {id} has been deleted.", 200
 
 
 if __name__ == "__main__":
